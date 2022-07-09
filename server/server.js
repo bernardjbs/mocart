7a4b6ac1543ddce32139753cb6c2485d95e0e181
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 04:55:15 +0800

    changed path to static folder for deployment in server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Deployment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
