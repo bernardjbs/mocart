98d91aac9fa5ad06d1892a975ab0f8732d06de19
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 22:21:12 +0800

    Fixed server production deployment code. MongoDB and React working on heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -12,4 +12,4 @@ mongoose.connect(
 mongoose.connection.on('connected', () => {
   console.log('Mongoose is connected');
 })
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; 
