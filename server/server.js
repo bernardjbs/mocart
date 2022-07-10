33efe84ea8db10a0680242f14b5ce6ecfd854240
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 20:27:09 +0800

    Pushing to heroku

--- server/server.js
+++ server/server.js
@@ -10,12 +10,10 @@ const app = express();
 // Deployment
 
 const dirname = path.resolve();
-console.log(dirname)
-console.log(__dirname)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
