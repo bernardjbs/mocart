5d692e5dc8f727238d19be80910286b7a15317c7
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 20:34:03 +0800

    Pushing to heroku

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
