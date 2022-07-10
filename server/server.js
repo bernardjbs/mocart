89bf48b78fe81c1faa7781b77340d6ad403d756e
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 20:31:19 +0800

    Pushing to heroku

--- server/server.js
+++ server/server.js
@@ -8,12 +8,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Deployment
-
-const dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
@@ -21,15 +19,14 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-
-
-
 db.once('open', () => {
+  const clientbuildpath = path.resolve('client', 'build', 'index.html')
   app.listen(PORT, () => {
-    console.log(`API server for mocart running on port ${PORT}!`);
+    console.log(`API server for mocart running on port ${PORT}! with path ${clientbuildpath}`);
   });
 });
