1f97639b9163de0aaab630bad5cccf68a2319d79
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-10 21:01:35 +0800

    pushing to heroku

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ const { User } = require('../models');
 module.exports = {
   // Get all users
   getUsers(req, res) {
+    console.log("hello");
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
