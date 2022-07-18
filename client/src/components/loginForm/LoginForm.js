48f2a4f73854063b648b8c2013498ef4c49583da
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-07-18 22:34:23 +0800

    pushing to heroku - testing login

--- client/src/components/loginForm/LoginForm.js
+++ client/src/components/loginForm/LoginForm.js
@@ -5,6 +5,9 @@ import Auth from '../../utils/auth';
 
 const URI = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_URI : process.env.REACT_APP_PROD_URI;
 
+// Test on local build
+// const URI = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_URI : http://www.localhost:5000;
+
 function LoginForm() {
   const [loginFormState, setLoginFormState] = useState([{ email: '', password: '' }]);
 
