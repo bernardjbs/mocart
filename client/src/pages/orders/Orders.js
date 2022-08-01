ee8501457cabf03cbe1e06af33b4cbe984b70078
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-08-02 02:12:30 +0800

    Could not resolve issue on heroku...

--- client/src/pages/orders/Orders.js
+++ client/src/pages/orders/Orders.js
@@ -14,35 +14,29 @@ function Orders() {
   const getUserOrders = async () => {
     try {
       const { data } = await axios.get(`${URI}/api/users`);
-      console.log(Array.isArray(data))
       let openOrders = [];
       let filteredOrders = [];
       let customerOrder = {};
-      if (Array.isArray(data)) {
-        data.map(user => {
-          if (user.orders.length > 0) {
-            const orders = user.orders;
-            orders.map(order => {
-              if (order.status === 'Open') {
-                openOrders.push(user);
-              }
-            });
-          };
-        });
-        const uniqueOrders = [...new Set(openOrders)]
-        uniqueOrders.map(order => {
-          customerOrder = {
-            id: order._id,
-            name: `${order.firstName} ${order.lastName}`,
-            orders: order.orders,
-          }
-          filteredOrders.push(customerOrder);
-        });
-        return [filteredOrders];
-      } else {
-        return [];
-      }
-
+      data.map(user => {
+        if (user.orders.length > 0) {
+          const orders = user.orders;
+          orders.map(order => {
+            if (order.status === 'Open') {
+              openOrders.push(user);
+            }
+          });
+        };
+      });
+      const uniqueOrders = [...new Set(openOrders)]
+      uniqueOrders.map(order => {
+        customerOrder = {
+          id: order._id,
+          name: `${order.firstName} ${order.lastName}`,
+          orders: order.orders,
+        }
+        filteredOrders.push(customerOrder);
+      });
+      return [filteredOrders];
     } catch (error) {
       throw error;
     }
